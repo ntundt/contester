c239850d63919ef6eba7263593ac30e98c387c94
Tsikhanovich Mikita <nikita.tihonovich@gmail.com>
2025-07-13 23:45:53 +0300

    Add files that I forgot to include during the previous commit

---
+++ contester/Data/Init/SqlServerInitDbContext.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace contester.Data.Init;
+
+public class SqlServerInitDbContext : DbContext, IInitDbContext
+{
+    private readonly ILogger<SqlServerInitDbContext> _logger;
+    public SqlServerInitDbContext(DbContextOptions<SqlServerInitDbContext> options,
+        ILogger<SqlServerInitDbContext> logger) : base(options)
+    {
+        _logger = logger;
+    }
+
+    private static string GetCheckInitNeededQuery()
+    {
+        return File.ReadAllText("Assets/Scripts/MSSQLServer/CheckInitNeeded.sql");
+    }
+
+    private static string GetInitScript()
+    {
+        return File.ReadAllText("Assets/Scripts/MSSQLServer/Init.sql");
+    }
+
+    private bool InitNeeded()
+    {
+        try
+        {
+            string query = GetCheckInitNeededQuery();
+            var aff = Database.SqlQueryRaw<int>(query).First();
+            return aff == 0;
+        } catch {
+            _logger.LogWarning("Could not connect to SQL Server to init the database");
+            return false;
+        }
+    }
+
+    public void Init()
+    {
+        if (!InitNeeded())
+        {
+            return;
+        }
+
+        _logger.LogInformation("Initializing SQL Server database");
+
+        try {
+            Database.ExecuteSqlRaw(GetInitScript());
+        } catch {
+            // ignore
+        }
+    }
+}
